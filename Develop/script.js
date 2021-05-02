4c675097a0905681bf8d9dad05494139592aeea8
jerismith32 <jerismith32@gmail.com>
2021-05-01 20:07:06 -0400

    updated the generate password information function to check for at least one charcter type to be selected

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,6 @@ var generateBtn = document.querySelector("#generate");
 //Password Element Variable Assignments
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
-//Should I make each number element a string?
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //var specialCharacters = ["!", """, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
@@ -14,6 +13,7 @@ var passwordLowerCase;
 var passwordUpperCase;
 var passwordNumbers;
 var passwordSpecials;
+var passwordInformation;
 
 // Write password to the #password input
 function writePassword() {
@@ -28,8 +28,8 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//Start Function to Generate Password Length
-function generatePasswordLength() {
+//Start Function to Generate User Input for desired password
+function generatePasswordInformation() {
     //User Input Needed for Password Length
     passwordLength = window.prompt("How many characters do you want your password to be? Please choose between 8 and 128 characters");
     console.log("User Input Password Length " + passwordLength);
@@ -40,16 +40,29 @@ function generatePasswordLength() {
     }
 
     else {
-      passwordLowerCase = window.confirm ("Will your password contain lower case letters? Choose 'OK' for yes and 'Cancel for no.");
+      //User Input Needed for Password containting Lowercase Letters
+      passwordLowerCase = window.confirm ("Will your password contain lowercase letters? Choose 'OK' for yes and 'Cancel for no.");
       console.log ("Lower Case Preferance " + passwordLowerCase);
-      passwordUpperCase = window.confirm ("Will your password contain upper case letters? Choose 'OK' for yes and 'Cancel for no.");
+      //User Input Needed for Password containting Uppercase Letters
+      passwordUpperCase = window.confirm ("Will your password contain uppercase letters? Choose 'OK' for yes and 'Cancel for no.");
       console.log ("Upper Case Preferance " + passwordUpperCase);
+      //User Input Needed for Password containting Numbers
       passwordNumbers = window.confirm ("Will your password contain numbers Choose 'OK' for yes and 'Cancel for no.");
       console.log ("Number Preferance " + passwordNumbers);
+      //User Input Needed for Password containting Special Characters
       passwordSpecials = window.confirm ("Will your password contain special characters? Choose 'OK' for yes and 'Cancel for no.");
       console.log ("Special Character Preferance " + passwordSpecials);
+    };
+
+    //Per Acceptance Criteria Validate at least one character type should be seclected. 
+    if (passwordLowerCase === false && passwordUpperCase === false && passwordNumbers === false && passwordSpecials === false ) {
+      passwordInformation= window.alert ("You must select at least one of the following criteria to be included in your password: lowercase letter, uppercase letter, number, or special character");
+      return generatePasswordInformation ();
     }
+    
 };
+
+
 
 
 
